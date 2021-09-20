dc81d2974ca7173b2dfb63293cd2434dcc5fbf1f
BenFrancka <benfrancka@gmail.com>
2021-09-20 11:24:16 -0700

    adds add inventory item function to services

---
+++ src/containers/InventoryCreateContainer.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import InventoryAddItemControls from '../components/inventory/InventoryAddItemControls';
+
---
+++ src/hooks/useAddtoInventory.js
@@ -0,0 +1,3 @@
+import { useEffect, useState } from 'react';
+import { addInventoryItem } from '../services/inventoryUtils';
+
