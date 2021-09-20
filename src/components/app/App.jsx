12a0b8d2d2473eb930e97f4ff322cb2d1e4bcfb7
BenFrancka <benfrancka@gmail.com>
2021-09-20 10:37:00 -0700

    adds file structure, sets up index.js and app.jsx with routes and a router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import InventoryListContainer from '../../containers/InventoryListContainer';
+// eslint-disable-next-line max-len
+import InventoryUpdateContainer from '../../containers/InventoryUpdateContainer';
+// eslint-disable-next-line max-len
+import InventoryCreateContainer from '../../containers/InventoryCreateContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+      <Route exact path="/:id">
+        <InventoryUpdateContainer />
+      </Route>
+      <Route exact path="/">
+        <InventoryListContainer />
+      </Route>
+      <Route exact path="/add">
+        <InventoryCreateContainer />
+      </Route>
+    </Switch>
+  );
 }
